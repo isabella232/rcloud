ee574ad83fcc2dac9e7c93b15bcc9ade4fd5a422
jabedford <32302360+JABedford@users.noreply.github.com>
2018-12-06 08:26:37 +0000

    Addition of combo box in import dialog

--- htdocs/js/ui/import_export.js
+++ htdocs/js/ui/import_export.js
@@ -82,7 +82,8 @@ RCloud.UI.import_export = (function() {
                                 $(['<p>Import notebooks from another GitHub instance.</p><p>Currently import does not preserve history.</p>',
                                    '<p>source repo api url:&nbsp;<input type="text" class="form-control-ext" id="import-source" style="width:100%;" value="https://api.github.com"></input></td>',
                                    '<p>notebooks:<br /><textarea class="form-control-ext" style="height: 20%;width: 50%;max-width: 100%" rows="10" cols="30" id="import-gists" form="port"></textarea></p>',
-                                   '<p>prefix (e.g. <code>folder/</code> to put notebooks in a folder):&nbsp;<input type="text" class="form-control-ext" id="import-prefix" style="width:100%;"></input>'].join('')));
+                                   '<p>prefix (e.g. <code>folder/</code> to put notebooks in a folder):&nbsp;<input type="text" class="form-control-ext" id="import-prefix" style="width:100%;"></input>',
+                                   '<p>Notebook Foreign Source:<br /><input type="text" class="form-control-ext" style="width:100%;" list="merge-notebook-gist" /><datalist id="merge-notebook-gist"<option value="gistone"><option value="gisttwo"></datalist></p>'].join('')));
 
                             var cancel = $('<span class="btn btn-cancel">Cancel</span>')
                                     .on('click', function() { $(dialog).modal('hide'); });
